250b91273229e9ed46adc8485a14d19027802130
Henrique Batista <hbshenrique.ti@gmail.com>
2026-03-12 10:04:53 -0300

    Primeiras tags de /dashboard

--- app/(pages)/dashboard/page.tsx
+++ app/(pages)/dashboard/page.tsx
@@ -1,5 +1,11 @@
 export default function Page() {
-    return (
-        <></>
-    )
+  return (
+    <div className="p-4 justify-center">
+      <h1 className="text-3xl font-bold">
+        Olá, (Cliente)      </h1>
+      <h2 className="opacity-60 ">
+        Gerencie as movimentações do seu plano de saúde
+      </h2>
+    </div>
+  );
 }
