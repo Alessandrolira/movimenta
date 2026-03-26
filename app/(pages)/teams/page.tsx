4edf558a1e5bbeba38c2b91a80c325265424957f
Henrique Batista <hbshenrique.ti@gmail.com>
2026-03-26 13:47:11 -0300

    Criação de teamcard e pagina de team por ID

--- app/(pages)/teams/page.tsx
+++ app/(pages)/teams/page.tsx
@@ -1,10 +1,11 @@
 "use client";
 
+import { TeamCard } from "@/app/components/TeamCard/TeamCard";
 import { api } from "@/services/api";
 import { useEffect, useState } from "react";
 
 export default function Page() {
-  const [teams, setTeams] = useState<{ id: string, nome: string}[]>([]);
+  const [teams, setTeams] = useState<{ id: string; nome: string }[]>([]);
 
   useEffect(() => {
     getEquipes();
@@ -22,7 +23,16 @@ export default function Page() {
 
   return (
     <div className="space-y-6 p-8">
-        <h1>Times {teams.length}</h1>
+      <h1>Equipes {teams.length}</h1>
+      {teams.length > 0 ? (
+        <div className="grid grid-cols-3">
+          {teams.map((team) => (
+            <TeamCard id={team.id} nome={team.nome} />
+          ))}
+        </div>
+      ) : (
+        <p>Nenhum equipe encontrada</p>
+      )}
     </div>
   );
 }
